029d28a00521c303b8c720eeb0922285dd962d7e
uri9050 <75596746+uri9050@users.noreply.github.com>
2023-07-05 11:35:53 -0600

    Actualizacion 05 de julio 2023

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+function toggleContent() {
+    var contenido = document.getElementById("contenido");
+    if (contenido.style.display === "none") {
+      contenido.style.display = "block";
+    } else {
+      contenido.style.display = "none";
+    }
+  }
+  
